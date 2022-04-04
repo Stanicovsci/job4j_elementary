6d0426e3e793dda4ddf170e11b8e64fd9f41a571
Stanicovsci <stanicovsci@gmail.com>
2022-04-04 10:55:37 +0300

    Info

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+
+        double first = Math.pow(x2 - x1, 2);
+        double second = Math.pow(y2 - y1, 2);
+        double rsl = Math.sqrt(first + second);
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+        double result1 = Point.distance(3, 3, 5, 3);
+        System.out.println("result (3, 3) to (5, 3) " + result1);
+        double result2 = Point.distance(4, 4, 6, 4);
+        System.out.println("result (4, 4) to (6, 4) " + result2);
+    }
+}
