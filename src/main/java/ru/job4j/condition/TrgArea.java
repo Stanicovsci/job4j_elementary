1a24634959661f801d3c68f61a960beedbf74c80
Stanicovsci <stanicovsci@gmail.com>
2022-04-08 09:54:46 +0300

    Info

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,13 +2,14 @@ package ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double p = 1/2 * (a + b + c);
+        double p = (a + b + c) / 2;
         double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return s;
+
     }
 
     public static void main(String[] args) {
-        double s = TrgArea.area(3, 3, 3);
-        System.out.println("area (3, 3, 3) = " + s);
+        double s = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + s);
     }
 }
